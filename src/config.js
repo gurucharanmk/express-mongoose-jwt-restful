8bcd2c812605d730cd6309a59ed6e600c64d853a
Gurucharan MK <gurucharanmk@users.noreply.github.com>
2018-04-01 19:16:31 +0530

    Setup database connectivity with server

--- src/config.js
+++ src/config.js
@@ -6,16 +6,34 @@ config();
 const development = {
   server: {
     port: parseInt(process.env.DEV_SERV_PORT)
+  },
+  db: {
+    uri: process.env.DEV_MONGODB_URI,
+    options: {
+      debug: true
+    }
   }
 };
 const production = {
   server: {
     port: parseInt(process.env.PROD_SERV_PORT)
+  },
+  db: {
+    uri: process.env.PROD_MONGODB_URI,
+    options: {
+      debug: false
+    }
   }
 };
 const test = {
   server: {
     port: parseInt(process.env.TEST_SERV_PORT)
+  },
+  db: {
+    uri: process.env.TEST_MONGODB_URI,
+    options: {
+      debug: false
+    }
   }
 };
 
